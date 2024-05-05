e8c02e6aaf61682c68c81272c7ab928ca1fe4c45
Bruno <bperez@munidigital.com>
2024-05-05 19:53:13 -0300

    Añade el memory_data

---
+++ instruction fetch.srcs/sim_1/new/test_memory_data.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.05.2024 19:38:03
+// Design Name: 
+// Module Name: test_memory_data
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_memory_data(
+
+    );
+endmodule
