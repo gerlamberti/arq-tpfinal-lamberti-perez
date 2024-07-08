4c89b00b4d19548a04c666abda8aac1873371724
gerlamberti <german.lamberti@unc.edu.ar>
2024-07-07 21:21:35 -0300

    Create text segment

--- source/pipeline/fetch/instruction_memory.v
+++ source/pipeline/fetch/instruction_memory.v
@@ -16,7 +16,26 @@ module instruction_memory
         output  reg     [NB-1:0]            o_instruction   
     );
     
-    reg     [NB-1  :0]     memory[TAM-1:0];
-    integer                   i;
+    reg     [NB-1:0]     memory[TAM-1:0];
+    integer              i;
+
+    // Crea el Text Segment
+    initial begin
+        for (i = 0; i < TAM; i = i + 1) begin
+            memory[i] = 0;
+        end  
+    end
+
+    //Pone a la salida la instruccion del senialada por el PC
+    always @(posedge i_clk) begin
+        if (i_step)begin
+            o_instruction  <= memory[i_pc];
+        end
+    end
+
+    // Crea el segmento de codigo que va arrecorrer el PC.
+    always @(posedge i_write_intruc) begin
+        memory[i_address_memory_ins] <= i_instruction; 
+    end
 
 endmodule
