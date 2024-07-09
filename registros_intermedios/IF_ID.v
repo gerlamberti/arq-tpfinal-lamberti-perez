b8f077fa5498bafd30cf878b966409d946a4b795
gerlamberti <german.lamberti@unc.edu.ar>
2024-07-09 19:51:11 -0300

    Creacion Registros intermedios y desarrollo inicial del decode

---
+++ registros_intermedios/IF_ID.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module IF_ID
+    #(
+        parameter NB = 32
+    )
+    (
+        input   wire                        i_clk,
+        input   wire                        i_reset,
+        //input   wire                        i_IF_ID_Write, // hazard unit
+        input   wire    [NBITS-1:0]         i_pc4,
+        //input   wire    [NBITS-1:0]         i_pc8,
+        input   wire    [NBITS-1:0]         i_Instruction,
+        input   wire                        i_step,
+        output  wire    [NBITS-1:0]         o_pc4,
+        //output  wire    [NBITS-1:0]         o_pc8,
+        output  wire    [NBITS-1:0]         o_instruction
+    );
+
+
+    reg     [NB-1:0] instruction;
+    reg     [NB-1:0] pc_4;
+    //reg     [NBITS-1:0] pc_8;
+       
+    always @(negedge i_clk)begin
+        if(i_reset)begin
+            instruction  <=   {NB{1'b0}};
+            pc_4         <=   {NB{1'b0}};
+            //pc_8         <=   {NBITS{1'b0}};
+        end
+        else if(i_step)begin //i_IF_ID_Write & i_step
+            instruction  <=   i_Instruction;
+            pc_4         <=   i_pc4;
+            //pc_8         <=   i_pc8;
+        end
+    end
+
+
+    assign o_instruction    =   instruction;
+    assign o_pc4            =   pc_4;
+    //assign o_pc8            =   pc_8;
+
+endmodule
---
+++ source/pipeline/decode/control_unit.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module control_unit #(
+        parameter NB = 6
+    )(
+        input   wire    [NBITS-1:0]   i_Instruction,
+        input   wire    [NBITS-1:0]   i_Special,
+        output  wire    [1   :   0]   o_ALUop,
+        output  wire                  o_ALUSrc
+    );
+
+    localparam ADDI   = 6'b001000;
+    localparam ANDI   = 6'b001100;
+
+    reg     [1:0]   ALUOp_Reg;
+    reg             ALUSrc_Reg;
+
+    always @(*) begin
+        case(i_Instruction)
+            ADDI:
+            begin
+                ALUOp_Reg           <=  2'b00   ;
+                ALUSrc_Reg          <=  1'b1    ;
+            end
+
+            ANDI:
+            begin
+                ALUOp_Reg           <=  2'b11   ;
+                ALUSrc_Reg          <=  1'b1    ;
+            end
+        endcase
+    end
+
+    assign  o_ALUSrc        =   ALUSrc_Reg;
+    assign  o_ALUOp         =   ALUOp_Reg;
+endmodule
