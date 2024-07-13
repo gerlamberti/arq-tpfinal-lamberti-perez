9910523c11c566478a85e6b5e098773918c7881d
Bruno <bperez@munidigital.com>
2024-07-13 20:52:10 -0300

    Cambio de nombre en la variable de entrada del ex

--- source/pipeline/execute/EXECUTE.v
+++ source/pipeline/execute/EXECUTE.v
@@ -11,7 +11,7 @@ module EXECUTE #(
     input                       i_alu_src,                 // 0 data_b, 1 immediate
     input       [       NB-1:0] i_data_a,
     input       [       NB-1:0] i_data_b,
-    input       [       NB-1:0] i_immediate,
+    input       [       NB-1:0] i_immediate_extended,      // Viene del decode, es el imm extendido
     output wire                 o_cero,
     output wire [       NB-1:0] o_alu_result
 );
@@ -20,12 +20,14 @@ module EXECUTE #(
   wire [NB_ALU_OP-1:0] w_operation;
 
 
-    mux2 #(.NB(NB)) mux_alu_src (
-        .i_data_a(i_data_b),
-        .i_data_b(i_immediate),
-        .i_sel(i_alu_src),
-        .o_data(w_data_b_or_immediate)
-    );
+  mux2 #(
+      .NB(NB)
+  ) mux_alu_src (
+      .i_data_a(i_data_b),
+      .i_data_b(i_immediate),
+      .i_sel(i_alu_src),
+      .o_data(w_data_b_or_immediate)
+  );
 
   alu_control #() alu_control (
       .i_funct_code(i_instruction_funct_code),
