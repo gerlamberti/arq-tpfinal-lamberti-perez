0aeb97409fbc2543881b7781856b194bfebf03a4
gerlamberti <german.lamberti@unc.edu.ar>
2024-07-27 19:33:59 -0300

    Implementamos BEQ, J. Anadi WORD SIZE.

--- source/pipeline/decode/control_unit.v
+++ source/pipeline/decode/control_unit.v
@@ -11,8 +11,13 @@ module control_unit #(
         output  reg                     o_mem_read,
         output  reg                     o_mem_write,
         output  reg                     o_reg_write,
-        output  reg    [1   :   0]      o_ExtensionMode
+        output  reg                     o_beq,
+        output  reg                     o_jump,
+        output  reg    [1   :   0]      o_ExtensionMode,
+        output  reg    [2   :   0]      o_word_size
     );
+
+
 
 // `define ADDI_OPCODE 6'b001000
 // `define ANDI_OPCODE  6'b001100 
@@ -25,6 +30,9 @@ module control_unit #(
                 o_mem_read        <=  1'b0;
                 o_mem_write       <=  1'b0 ;
                 o_reg_write       <=  1'b0 ;
+                o_beq             <=  1'b0 ;
+                o_jump            <=  1'b0 ;
+                o_word_size       <=  3'b000;
             end
 
             `ADDI_OPCODE: begin
@@ -33,6 +41,9 @@ module control_unit #(
                 o_mem_read        <=  1'b0;
                 o_mem_write       <=  1'b0;
                 o_reg_write       <=  1'b1;
+                o_beq             <=  1'b0 ;
+                o_jump            <=  1'b0 ;
+                o_word_size       <=  3'b000;
             end
 
             `ANDI_OPCODE: begin
@@ -41,6 +52,29 @@ module control_unit #(
                 o_mem_read        <=  1'b0;
                 o_mem_write       <=  1'b0;
                 o_reg_write       <=  1'b1;
+                o_beq             <=  1'b0 ;
+                o_jump            <=  1'b0 ;
+                o_word_size       <=  3'b000;
+            end
+            `BEQ_OPCODE: begin
+                o_ALUSrc          <=  `RT_ALU_SRC;
+                o_ExtensionMode   <=  `SIGNED_EXTENSION_MODE;
+                o_mem_read        <=  1'b0;
+                o_mem_write       <=  1'b0;
+                o_reg_write       <=  1'b0;
+                o_beq             <=  1'b1 ;
+                o_jump            <=  1'b0 ;
+                o_word_size       <=  3'b000;
+            end
+            `JUMP_OPCODE: begin
+                o_ALUSrc          <=  `RT_ALU_SRC;
+                o_ExtensionMode   <=  `SIGNED_EXTENSION_MODE;
+                o_mem_read        <=  1'b0;
+                o_mem_write       <=  1'b0;
+                o_reg_write       <=  1'b0;
+                o_beq             <=  1'b0 ;
+                o_jump            <=  1'b1 ;
+                o_word_size       <=  3'b000;
             end
             default: begin    
                 o_ALUSrc          <=  `RT_ALU_SRC;
@@ -48,6 +82,9 @@ module control_unit #(
                 o_mem_read        <=  1'b0;
                 o_mem_write       <=  1'b0 ;
                 o_reg_write       <=  1'b0 ;
+                o_beq             <=  1'b0 ;
+                o_jump            <=  1'b0 ;
+                o_word_size       <=  3'b000;
             end
         endcase
     end
