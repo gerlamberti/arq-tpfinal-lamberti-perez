b2bf4d851fcf7d2dc0d7260f39152d3ea95845a1
Bruno <bperez@munidigital.com>
2024-08-16 23:27:23 -0300

    TERMINA EL TRABAJO LPM AGUANTE TALLERES

--- client/ejemplo_jalr.asm
+++ client/ejemplo_jalr.asm
@@ -2,10 +2,10 @@ nop # PC = 4
 addi $7,$0,36 # PC = 8
 addi $1,$0,4 # PC = 12
 addi $2,$1,4 # PC = 16
-jalr $7 # PC = 20  Chequear que $31 = 28
+jalr $7 # PC = 20  salta al 36 Chequear que $31 = 28
 nop # PC = 24
 addi $6,$0,2 # PC = 28
-addi $7,$0,17 # PC = 32
+addi $7,$0,17 # PC = 32 No tiene que ejecutar
 addi $8,$0,18 # PC = 36
 addi $9,$0,29 # PC = 40
 addi $10,$0,100 # PC = 44
